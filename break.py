7b4cde5adfefb7141f24cfbd9f8a43f4142ec3e2
Yasin Alpul <yalpul@gmail.com>
2018-08-04 15:15:10 +0300

    version 1.0 ready

--- break.py
+++ break.py
@@ -4,7 +4,7 @@ import cv2
 import torch
 from divDigits import separate_image
 
-model = torch.load('model.pt')
+model = None
 
 def break_captcha(image):
 	digits = [[cv2.resize(img, (14, 24))] for img in separate_image(image)]
@@ -12,11 +12,18 @@ def break_captcha(image):
 	with torch.no_grad():
 		result = model(tens)
 		_, nums = torch.max(result, 1)
+
 	out = [num.item() for num in nums]
-	print(out[0], out[2], out[4], out[1], out[3], out[5])
+
+	captcha = [out[0], out[2], out[4], out[1], out[3], out[5]]
+	captcha = ''.join([str(i) for i in captcha])
+	print(captcha)
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		print('Usage: python break.py <image_file1> [image_file2 ...]')
+		sys.exit(-1)
+
+	model = torch.load('model.pt')
 	for i in sys.argv[1:]:
 		break_captcha(i)
