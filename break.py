ee6c40b26445afafc06e564208326e8c7a556434
Yasin Alpul <yalpul@gmail.com>
2018-08-04 14:31:51 +0300

    captcha breaker added

---
+++ break.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import sys
+import cv2
+import torch
+from divDigits import separate_image
+
+model = torch.load('model.pt')
+
+def break_captcha(image):
+	digits = [[cv2.resize(img, (14, 24))] for img in separate_image(image)]
+	tens = torch.tensor(digits, dtype=torch.float32).cuda() / 255
+	with torch.no_grad():
+		result = model(tens)
+		_, nums = torch.max(result, 1)
+	out = [num.item() for num in nums]
+	print(out[0], out[2], out[4], out[1], out[3], out[5])
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		print('Usage: python break.py <image_file1> [image_file2 ...]')
+	for i in sys.argv[1:]:
+		break_captcha(i)
