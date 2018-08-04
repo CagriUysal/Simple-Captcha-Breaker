98b08202943d50de3a02688a49f20b25aef57eb6
Yasin Alpul <yalpul@gmail.com>
2018-08-04 20:09:04 +0300

    breaker now runs on CPU instead of GPU

--- breaker.py
+++ breaker.py
@@ -4,11 +4,11 @@ import cv2
 import torch
 from divDigits import separate_image
 
-model = None
+model = torch.load('model.pt', map_location='cpu')
 
 def break_captcha(image):
 	digits = [[cv2.resize(img, (14, 24))] for img in separate_image(image)]
-	tens = torch.tensor(digits, dtype=torch.float32).cuda() / 255
+	tens = torch.tensor(digits, dtype=torch.float32) / 255
 	with torch.no_grad():
 		result = model(tens)
 		_, nums = torch.max(result, 1)
@@ -17,13 +17,12 @@ def break_captcha(image):
 
 	captcha = [out[0], out[2], out[4], out[1], out[3], out[5]]
 	captcha = ''.join([str(i) for i in captcha])
-	print(captcha)
+	return captcha
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
-		print('Usage: python break.py <image_file1> [image_file2 ...]')
+		print('Usage: python breaker.py <image_file1> [<image_file2> ...]')
 		sys.exit(-1)
 
-	model = torch.load('model.pt')
 	for i in sys.argv[1:]:
-		break_captcha(i)
+		print(break_captcha(i))
